8fc046bf947a843e5eb522bff14f53f7e50ac1c8
AlmohaimeedAbdullah <abdullah.almouhamid.jed.k1@tuwaiq.edu.sa>
2021-10-28 09:50:03 +0300

    creation date

---
+++ app/src/main/java/com/tuwaiq/todolistproject/model/UserDao.kt
@@ -0,0 +1,4 @@
+package com.tuwaiq.todolistproject.model
+
+interface UserDao {
+}
--- app/src/main/java/com/tuwaiq/todolistproject/model/UserData.kt
+++ app/src/main/java/com/tuwaiq/todolistproject/model/UserData.kt
@@ -1,5 +1,8 @@
 package com.tuwaiq.todolistproject.model
 
+import androidx.room.Entity
+
+@Entity
 data class UserData (
     var txtTitle :String,
     var txtDate :String,
